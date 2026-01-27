a8bf997b3c89fc49ddbe46efeb7a040137cd2718
Rashmi Gupta <rashmigupta9347@gmail.com>
2026-01-27 19:23:44 +0530

    Add root home page

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Pastebin Lite</h1>
+      <p>Your Pastebin Lite service is running.</p>
+      <p>Create a paste via API and view it using /p/&lt;id&gt;</p>
+    </main>
+  );
+}
