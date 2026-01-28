d99bbcad941830923fc2c5d0d0a16e89deefbe18
Rashmi Gupta <rashmigupta9347@gmail.com>
2026-01-28 12:12:13 +0530

    Fix import paths and config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // This allows the build to finish even if there are linting errors
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // This allows the build to finish even if there are type errors
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
