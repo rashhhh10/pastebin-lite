d4c08ab2d3630a214e915ca5398f42a3c73dd0cd
Rashmi Gupta <rashmigupta9347@gmail.com>
2026-01-28 12:31:13 +0530

    Fix next config and disable linting

--- app/api/pastes/route.js
+++ app/api/pastes/route.js
@@ -1,4 +1,4 @@
-import { redis } from "../../../lib/redis";
+import { redis } from "@/lib/redis";
 
 import crypto from "crypto";
 
--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // This allows the build to finish even if there are type errors
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
